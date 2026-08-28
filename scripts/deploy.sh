3c1b6cb0eb8f52ed69434c26856510c03bfe1db3
alessiogandelli <alessiogandelli99@gmail.com>
2026-08-28 17:04:07 +0200

    chore: script deploy e bump piattaforma iOS a 17.0

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+flutter build ipa   --release
+
+# ipa path 
+IPA_PATH='/Users/alessiogandelli/dev/cantiere/fight-camp/fight_camp_app/build/ios/ipa/fight_camp.ipa'
+
+APP_STORE_USERNAME="ganformatica@gmail.com"
+gi
+
+xcrun altool --validate-app --file "$IPA_PATH" --type "ios" --username "$APP_STORE_USERNAME" --password @keychain:"appStoreConnect: $APP_STORE_USERNAME"
+xcrun altool --upload-app --file "$IPA_PATH" --type "ios"  --username "$APP_STORE_USERNAME" --password @keychain:"appStoreConnect: $APP_STORE_USERNAME" 
+
+
